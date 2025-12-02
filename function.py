3250dda30701cf8e274528f14c6467e5a88da7eb
Rehan Khan <armankhanladwanrhs@gmail.com>
2025-12-02 18:02:46 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,56 @@
+n=int(input())
+def square():
+    print(n**2)
+
+square()
+def square(n):
+    print(n**3)
+square(n) 
+def s(n):
+    return n*n
+
+print(s(n)):  
+a=int(input())
+b=int(input())
+def add(a,b):
+    s=a+b
+    return s
+x=add(a,b)
+print(x)
+num=int(input())
+def check(num):
+    if num%2==0:
+        return "Even"
+    else:
+        return 'Odd'
+    
+x=check(n)
+print(x)
+title=int(input())
+author=int(input())
+year=int(input())
+def book_details()
+name=str(input())
+message=str(input())
+def greet(name,message):
+    return(name,message)
+s=greet(name,message)
+s1=greet(message=message,name=name)
+print(s)
+print(s1)
+name=input()
+age=int(input)
+city=input()
+num=int(input())
+def isprime(n):
+    if n==0 or n==1:
+        return False
+    for i in range(2,n):
+        if n%i==0:
+            return False
+        return True
+    
+print(isprime(num))  
+for i in range(2,num+1):
+    if isprime(i):
+        print(i,end=' ')
